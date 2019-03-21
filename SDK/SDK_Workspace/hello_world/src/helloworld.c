58bab02f281d069477316dd8bd47e524d9cf72a1
Marko <sasa-bojanic@hotmail.com>
2019-03-21 10:26:44 +0100

    Uradjeno do 37. slajda

--- SDK/SDK_Workspace/hello_world/src/helloworld.c
+++ SDK/SDK_Workspace/hello_world/src/helloworld.c
@@ -30,17 +30,30 @@
  *   uartlite    Configurable only in HW design
  *   ps7_uart    115200 (configured by bootrom/bsp)
  */
+#define XPAR_MY_PERIPHERAL_0_BASEADDR 0x7DE00000
+#define XPAR_MY_PERIPHERAL_0_HIGHADDR 0x7DE0FFFF
 
 #include <stdio.h>
 #include "platform.h"
-
-void print(char *str);
-
-int main()
-{
-    init_platform();
-
-    print("Hello World\n\r");
-
-    return 0;
+#include "xparameters.h"
+#include "xio.h"
+int main () {
+unsigned int DataRead;
+unsigned int OldData;
+init_platform();
+// Clear the screen
+xil_printf("%c[2J",27);
+OldData = (unsigned int) 0xffffffff;
+while(1){
+// Read the state of the DIP switches
+DataRead = XIo_In32(XPAR_MY_PERIPHERAL_0_BASEADDR);
+// Send the data to the UART if the settings change
+if(DataRead != OldData){
+xil_printf("DIP Switch settings: 0x%2X\r\n", DataRead);
+// Set the LED outputs to the DIP switch values
+XIo_Out32(XPAR_MY_PERIPHERAL_0_BASEADDR, DataRead);
+// Record the DIP switch settings
+OldData = DataRead;
+}
+}
 }
---
+++ drivers/my_timer_v1_00_a/src/my_timer.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          D:\RA179-2016\lab3/drivers/my_timer_v1_00_a/src/my_timer.c
+* Version:           1.00.a
+* Description:       my_timer Driver Source File
+* Date:              Thu Mar 21 10:18:07 2019 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "my_timer.h"
+
+/************************** Function Definitions ***************************/
+
